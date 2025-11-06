6caa59f173552396ab2395459034df3109725079
Sampath1071 <sampathmammula742@gmail.com>
2025-11-06 20:53:33 +0530

    Refactor not_attack predicate for clarity

--- prolog.pl
+++ prolog.pl
@@ -1,13 +1,23 @@
+% 8 Queens Problem in Prolog
+% --------------------------
+
+% Main predicate
 queens(Queens) :-
     permutation([1,2,3,4,5,6,7,8], Queens),
     safe(Queens).
+
+% Base case
 safe([]).
+% Recursive check
 safe([Queen|Queens]) :-
     safe(Queens),
     not_attack(Queen, Queens, 1).
+
+% Base case for not_attack
 not_attack(_, [], _).
+% Recursive check for diagonal safety
 not_attack(Queen, [Other|Others], Distance) :-
-    Queen =\= Other,
-    abs(Queen - Other) =\= Distance,
+    Queen =\= Other,                        % not same row
+    abs(Queen - Other) =\= Distance,        % not same diagonal
     NewDistance is Distance + 1,
     not_attack(Queen, Others, NewDistance).
