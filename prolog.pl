7310899fd863e4ac00cb4e1fbbbf0937bd1f9afe
Sampath1071 <sampathmammula742@gmail.com>
2025-11-06 20:51:50 +0530

    Fix formatting and remove unnecessary line breaks

--- prolog.pl
+++ prolog.pl
@@ -1,12 +1,10 @@
 queens(Queens) :-
     permutation([1,2,3,4,5,6,7,8], Queens),
     safe(Queens).
-
 safe([]).
 safe([Queen|Queens]) :-
     safe(Queens),
     not_attack(Queen, Queens, 1).
-
 not_attack(_, [], _).
 not_attack(Queen, [Other|Others], Distance) :-
     Queen =\= Other,
